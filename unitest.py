d9a6dd490ba6552faa98490dcbf6406a0f37fecb
jassemb <benalijassem201@gmail.com>
2022-11-23 15:09:47 +0100

    test

--- unitest.py
+++ unitest.py
@@ -11,7 +11,6 @@ class AppTestCase(unittest.TestCase):
     def tearDown(self):
         self.ctx.pop()
 
-
     def test_landing_aliases(self):
         landing = self.client.post('/')
         assert landing.status_code == 405
@@ -22,6 +21,9 @@ class AppTestCase(unittest.TestCase):
         assert "<h1 style=\"padding:10px\">Stock News Sentiment Analyzer</h1>" in html
         landing.status_code == 200
 
+    def test_textfiled_correct(self):
+            response = self.client.get('/', data=dict(text_field='AMZN'))
+            self.assertEqual(response.status_code, 200)
 
 if __name__ == "__main__":
     unittest.main()
